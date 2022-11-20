9a403e747d907b3c74726c1b94d361253690d236
tokatu4561 <ryobsk4561@gmail.com>
2022-11-20 15:22:21 +0900

    buble sort

--- bubble-sort/index.ts
+++ bubble-sort/index.ts
@@ -1,19 +1,20 @@
-import { TaskList } from "./concrete";
 
-// 集約体(aggregate)に依存しないことで、
-// 使用している側で、実装内容を意識せずに済む
-// 使用している側は hasNextとnextを使用するので、その中身が変わっても気にすることはない
+// 計算量 平均 O(n^2) best O(n) worst O(n^2)
+const bubbleSort = (nums: Array<number>): Array<number>  => {
+  const len = nums.length
+  const sortNums = nums
 
-const agr = new TaskList([]);
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; i < len - 1 - i; j++) {
+      if (sortNums[j] > sortNums[j+1]) [sortNums[j], sortNums[j+1]] = [sortNums[j+1], sortNums[j]]
+    }
+  }
 
-agr.add({ name: "タスク1" });
-agr.add({ name: "タスク2" });
-agr.add({ name: "タスク3" });
-agr.add({ name: "タスク4" });
-
-const itr = agr.createIterater();
-
-while (itr.hasNext()) {
-  const task = itr.next();
-  console.log(task);
+  return sortNums
 }
+
+
+const nums = [1, 5, 6, 7, 8]
+const sortedNums = bubbleSort(nums)
+
+console.log(sortedNums)
--- bubble-sort/iterater.ts
+++
@@ -1,9 +0,0 @@
-export interface Iterater<T> {
-  // Iterater は 集約体 (Aggregate)に次の要素が存在するかしないかと、次の要素を返す
-  next(): T;
-  hasNext(): boolean;
-}
-
-export interface Aggregate<T> {
-  createIterater(): Iterater<T>;
-}
