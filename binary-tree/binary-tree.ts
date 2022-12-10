9fc47fb8d5df3768bbada927d1ba085d8331f922
tokatu4561 <ryobsk4561@gmail.com>
2022-12-10 22:48:42 +0900

    search inorder

--- binary-tree/binary-tree.ts
+++ binary-tree/binary-tree.ts
@@ -23,3 +23,25 @@ export const insert = (node: Node | undefined, value: number): Node => {
 
     return node
 }
+
+// 小さい順に表示する
+export const inorder = (node: Node | undefined): void => {
+    if (node) {
+        inorder(node.left)
+        console.log(node.value)
+        inorder(node.right)
+    }
+} 
+
+// 検索
+export const search = (node: Node | undefined, value: number): boolean => {
+    if (!node) return false
+
+    if (node.value > value) {
+        return search(node.left, value)
+    } else if (node.value < value) {
+        return search(node.right, value)
+    }
+
+    return true
+} 
