a58373f9fc1ee7bdf830037b16d4cb596a9e750e
tokatu4561 <ryobsk4561@gmail.com>
2022-12-13 20:29:23 +0900

    to class

--- binary-tree/index.ts
+++ binary-tree/index.ts
@@ -1,6 +1,15 @@
-import { insert } from "./binary-tree";
+import { BinarySearchTree } from "./binary-tree";
 
-let root
-insert(root, 3)
-insert(root, 6)
-insert(root, 4)
+const tree = new BinarySearchTree()
+tree.insert(3)
+tree.insert(6)
+tree.insert(5)
+tree.insert(7)
+tree.insert(1)
+tree.insert(10)
+tree.insert(2)
+tree.inorder()
+console.log(tree.search(6)) // true
+console.log(tree.search(12)) // false
+tree.remove(6)
+tree.inorder()
